352603cdbe02f3d2c2ed1b22c2725fdd6fb9bf54
maryehuerta <mary.huerta@mavs.uta.edu>
2017-08-30 23:16:36 -0500

    added clock

--- src/containers/Clock.js
+++ src/containers/Clock.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Clock extends Component {
+	constructor(props) {
+		super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) 
+    };
+	}
+	componentDidMount() {
+    this.intervalID = setInterval( () => this.setTime(), 1000 );
+	}
+	componetWillUnmount(){
+		clearInterval(this.intervalID);
+	}
+	setTime() {
+		this.setState( {time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) } );  
+	}
+	
 	render() {
 		return(
-			<div> Yo </div>
+			<div className="Clock">
+				{this.state.time}
+			</div>
 		);
 	}
 }
