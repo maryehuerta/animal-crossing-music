51092e7ed24cd61de5f4c88dacd36b2e0551930f
maryehuerta <mary.huerta@mavs.uta.edu>
2018-10-05 12:42:38 -0500

    Got rid of music and clouds

--- src/components/backgrounds.js
+++ src/components/backgrounds.js
@@ -5,16 +5,9 @@ export default class Background extends Component {
     
     return(
       <div>
-        <div id="clouds">
-          <div className="cloud x1"></div>
-          <div className="cloud x2"></div>
-          <div className="cloud x3"></div>
-          <div className="cloud x4"></div>
-          <div className="cloud x5"></div>
-        </div>
         <div className="sign"> <img className="logoImg" src={require("../images/Animal_Crossing_Logo.png")}  alt="sign"/> </div>
         <div className="globe-div">
-          <img className="globe" src={require("../images/GlobeAnimalCrosssingMusic.png")} alt="globe"  />
+          <img id="loading" className="globe" src={require("../images/GlobeAnimalCrosssingMusic.png")} alt="globe"  />
         </div>
       </div>
     );
